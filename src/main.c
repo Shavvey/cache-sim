ee42f1ea4819c0aaa27b25ed88ce1b4eb066e56e
Cole Johnson <coledjohn@gmail.com>
2024-02-26 16:06:00 -0700

    adding makefile

--- src/main.c
+++ src/main.c
@@ -1,10 +1,17 @@
 #include <stdlib.h>
 #include "file_reader.h"
+#define CONFIG_FILE 2
+#define TRACE_FILE 3
 
 int main(int argc, char *argv[]){
   // argc will track to see if a arguments have been passed 
-  if(argc > 1 ){
+  // 1: name of textfile
+  // 2: name of cache config file
+  // 3: name of load-store instruction trace file?
+  if(argc > 2 ){
+    
 
   }
+  // exit out of program with success code
   return EXIT_SUCCESS;
 }
