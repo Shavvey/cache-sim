191f469accc0f4c6596675f999a6edcb8335eeb2
Cole Johnson <coledjohn@gmail.com>
2024-02-26 17:38:09 -0700

    creating cache config struct

--- src/cache.c
+++ src/cache.c
@@ -1,5 +1,33 @@
 #include "file_reader.h"
+#include <stdint.h>
 
-/* cache_t caputes the all the important information of the cache setup
- * INCLUDES THE FOLLOWING:*/
-struct cache_t {};
+/* cache_t captures the all the important information of the cache setup
+ * INCLUDES THE FOLLOWING:
+ * LINE SIZE: Specifies the line (block) size for the cache in bytes. Should be
+ * a non-negative power of 2
+ * ASSOCIATIVITY: Specifies the associativity of the cache, a '1' implies a
+ * direct mapped cache, while a '0' implies a fully-associated one
+ * DATA SIZE: Specifies the total size of the data in the cache, should be
+ * specified in KB. For example, a value of '64' means a 64KB cache
+ * REPLACEMENT POLICY: Specifies the replacement policy for the cache.
+ * Currently, there's is only FIFO, specified by a '1' and Random, specified by
+ * a '0'
+ * MISS PENALTY: Specifies the number of cycles penalized on a cache miss. May
+ * be any postive integer.
+ * WRITE ALLOCATE: Specifies the cache policy on write misses. A value of '0'
+ * indicates no-write-allocate. A value of '1' indicates write-allocate
+ * Assumes write through is used with no-write-allocate and write-back is used
+ * with write-allocate.*/
+// represent replacement policy
+enum REP_POLICY { RANDOM = 0, FIFO = 1 };
+// represent whether we have no-write-allocate or write-allocate cache
+enum WRITE_ALLOC { NO = 0, YES = 1 };
+
+struct cache_t {
+  uint32_t block_size;
+  uint32_t associativity;
+  uint32_t cache_size;
+  enum REP_POLICY rep_policy;
+  uint32_t miss_penalty;
+  enum WRITE_ALLOC write_alloc;
+};
--- src/file_reader.h
+++ src/file_reader.h
@@ -4,7 +4,6 @@ extern ssize_t bytes;
 extern char *line_buf;
 extern size_t len;
 /*FUNCTION DECLARATIONS*/
-FILE* get_file(char *name);
+FILE *get_file(char *name);
 void handle_byte_error(ssize_t bytes);
 const char *read_line(FILE *file);
-
