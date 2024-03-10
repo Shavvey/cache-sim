6345e8557510330ed1c6c4c882e5a0c13d631f18
Cole Johnson <coledjohn@gmail.com>
2024-03-09 23:54:33 -0700

    compiling to x86 using gcc

--- src/sim.h
+++ src/sim.h
@@ -11,6 +11,7 @@ struct blocks_t {
   // also contains a reference to the next block
   struct blocks_t *next;
 };
+
 // each set holds a given amount of cache blocks, given by associativity value
 struct set_t {
   // each set is represented as a chain of cache blocks
@@ -28,7 +29,6 @@ struct cache {
 void load_cache_config(char *config_fname);
 bool compare_tag(struct cache_addr_d addr, uint32_t address1,
                  uint32_t address2);
-
 uint32_t make_bit_mask(uint8_t num_bits, uint8_t offset);
 void cache_sim(char *fname);
 void record_hit(enum access_type type);
